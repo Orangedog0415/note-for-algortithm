a811292ea15e02ecc714201fa562e5718969dfd4
Orangedog0415 <orangedog0415@outlook.com>
2026-03-03 23:59:16 +0800

    机器人走格子

--- luogu/机器人走格子.cpp
+++ luogu/机器人走格子.cpp
@@ -9,17 +9,22 @@ struct info{
     int diridx;
 };
 
+enum Direction{
+    S = 0, E = 1, N = 2, W = 3
+};
+
 int s[55][55];//square
 int p[55][55];//point
 int n, m;
 int dirx[4] = {1, 0, -1, 0};
 int diry[4] = {0, 1, 0, -1};
+//0123:x+,y+,x-,y-
 
-void dfs(int x, int y, int desx, int desy, int cost, int diridx){
+//void dfs(int x, int y, int desx, int desy, int cost, int diridx){
     //1边界返回
     //2到达返回
     //循环，
-}
+//}
 
 int main(){
     cin >> n >> m;
@@ -33,6 +38,45 @@ int main(){
         }
     }
 
+    int stax, stay, desx, desy, dir;
+    char d;
+    cin >> stax >> stay >> desx >> desy >> d;
+    dir = (Direction)d;
+
     queue<info> q;
+    int ans = 0;
+    info start = {stax + 1, stay + 1, 0, dir};
+    q.push(start);
+    while(q.size()){
+        info t = q.front(); q.pop();
+        
+        if(t.x < 0 || t.y < 0 || t.x > n || t.y > m) continue;
+
+        if(t.x == desx + 1 && t.y == desy + 1){
+            ans = t.cost;
+            break;
+        }
+
+        for(int i = 1; i <= 5; i++){
+            info s;
+            if(1 <= i && i <= 3){
+                s = {t.x + i*dirx[t.diridx], t.y + i*diry[t.diridx], t.cost, t.diridx};
+            }
+            else if(i == 4){
+                s = t;
+                s.diridx = (s.diridx + 1) % 4;
+            }
+            else{
+                s = t;
+                s.diridx = (s.diridx + 3) % 4;
+            }
+            s.cost++;
+            if(s.x >= 0 && s.y >= 0 && s.x <= n && s.y <= m){
+                q.push(s);
+            }
+        }
+    }
+
+    cout << ans;
     
 }
