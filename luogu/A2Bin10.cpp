b9349d1a493f28217ba40038ef7174c4dca6f847
Orangedog0415 <orangedog0415@outlook.com>
2026-03-04 20:04:06 +0800

    其实是bfs。

--- luogu/A2Bin10.cpp
+++ luogu/A2Bin10.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <set>
+#include <unordered_set>
 using namespace std;
 
 vector<string> vecA, vecB;
+int sz;
 
-void dfs(string a, string b, int step){
-    if(step > 10) return;
+int dfs(string a, string b, int step){
+    if(step > 10) return 11;
 
-    
+    if(a == b) return step;
 
+    int ans = 11;
+    for(int i = 0; i < sz; i++){
+        size_t pos = a.find(vecA[i]);
+        if(pos != string::npos){
+            a.replace(pos, vecA[i].length(), vecB[i]);
+            ans = min(dfs(a, b, step+1), ans);
+            a.replace(pos, vecB[i].length(), vecA[i]);
+        }
+    }
+
+    return ans;
 }
 
 int main(){
@@ -21,5 +36,40 @@ int main(){
         vecB.push_back(Bi);
     }
 
-    dfs(A, B, 0);
+    sz = (int)vecA.size();
+    queue<pair<string, int>> q;
+    unordered_set<string> st;
+
+    q.push({A, 0});
+    st.insert(A);
+    while(q.size()){
+        auto t = q.front(); q.pop();
+        string a = t.first;
+        int cost = t.second;
+
+        if(cost >= 10) continue;
+
+        for(int i = 0; i < sz; i++){
+            size_t pos = 0;
+
+            while((pos = a.find(vecA[i], pos)) != string::npos){
+                string cur = a;
+                cur.replace(pos, vecA[i].length(), vecB[i]);
+
+                if(cur == B){
+                    cout << cost + 1;
+                    return 0;
+                }
+
+                if(st.find(cur) == st.end()){
+                    q.push({cur, cost + 1});
+                    st.insert(cur);
+                }
+
+                pos++;
+            }
+        }
+    }
+
+    cout << "NO ANSWER!";
 }
