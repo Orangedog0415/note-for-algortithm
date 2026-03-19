c911963d9d5ddcd9c09c17240a73e26a81898433
orangedog0415 <orangedog0415@outlook.com>
2026-03-20 00:36:00 +0800

    vault backup: 2026-03-20 00:36:00

---
+++ 24蓝桥/C.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+const int N = 100010;
+
+int main(){
+    int n;
+    long long s;
+    cin >> n >> s;
+    double per = double(s)/n;
+
+    vector<pair<int, int>> a(N);
+
+}
